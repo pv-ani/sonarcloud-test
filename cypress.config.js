e8acef36f096f120a0b56786a21403e5f3cf2ad2
pv-ani <159751974+pv-ani@users.noreply.github.com>
2024-04-22 13:16:50 +0530

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,18 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+  defaultCommandTimeout: 40000,
+  videoCompression: true,
+  chromeWebSecurity: false,
+  reporter: 'cypress-mochawesome-reporter',
+  requestTimeout: 40000,
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'custom-title',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+  },
   e2e: {
     setupNodeEvents() {
       // implement node event listeners here
